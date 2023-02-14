6c57e5177de45e36d4d084c50c3caab40e070b7e
Celeste Bareiro <cnyx7@proton.me>
2023-02-14 03:19:43 +0000

    My Domain Name Generator using JavaScript

--- src/app.js
+++ src/app.js
@@ -8,4 +8,31 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("button").addEventListener("click", ejecutar);
+};
+
+let pronoun = ["the", "our", "my"];
+let adj = ["great", "big", "silent"];
+let noun = ["jogger", "racoon", "sun"];
+let domain = [".com", ".org", ".edu"];
+
+const ejecutar = () => {
+  let texto;
+  let parrafo;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      //console.log(pronoun[i] + adj[j]);
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < noun.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+          parrafo = document.createElement("p");
+          texto = document.createTextNode(
+            pronoun[i] + adj[j] + noun[k] + domain[l]
+          );
+          parrafo.appendChild(texto);
+          document.querySelector("#resultados-dominios").appendChild(parrafo);
+        }
+      }
+    }
+  }
 };
